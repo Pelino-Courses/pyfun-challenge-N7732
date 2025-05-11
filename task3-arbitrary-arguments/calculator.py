9be73b7a995687d5e8f73c1888aa947fad5dbea0
NSHIMYUMUREMYI Olivier <oliviernshimyumuremyi4@gmail.com>
2025-05-11 18:38:08 +0200

    Aribitrary argument

---
+++ task3-arbitrary-arguments/calculator.py
@@ -0,0 +1,43 @@
+print("==welcome==")
+def calculate(*args,**kwargs):
+    for x in args:
+        if not isinstance(x,(int,float)):
+            raise ValueError("Invalid input")
+    if not args:
+          raise ValueError("No Number")
+    Operation=["Add","Multiply"]
+    get_operation=[y for y in Operation if kwargs.get(y)]
+    if len(get_operation) !=1:
+         raise ValueError
+    Operation=get_operation[0]
+    return process(Operation, *args)
+def process(Operation, *args):
+      if Operation=="Add":
+            return sum(args)
+      if Operation=="Multiply":
+         result=1
+         for number in args:
+            result*=number
+         return result
+    
+def main():
+    try:
+        print("Choose Operation:\n1.Add\n2.Multiply")
+        choice=input("Enter your choice : ").strip()
+        Numbers=input("Enter numbers separated by space : ").split()
+        input_Number=[float(x) for x in Numbers]
+        if choice=="1":
+          result = calculate(*input_Number, Add=True)
+        elif choice=="2":
+          result = calculate(*input_Number, Multiply=True)
+        else:
+            raise ValueError("Invalid option")
+        print(f"Result : {result}")
+    
+    except Exception as e:
+        print(f"Error : {e}")
+if __name__=="__main__":
+    main()
+          
+
+      
